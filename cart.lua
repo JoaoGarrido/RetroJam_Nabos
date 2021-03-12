312e9f2687d7289faddfad6720b0ace6f4aec82f
ZeD4805 <zed4805@gmail.com>
2021-03-12 21:04:50 +0000

    ay

--- cart.lua
+++ cart.lua
@@ -4,25 +4,6 @@
 -- script: lua
 
 t=0
-x=96
-y=24
-
-
-
-function TIC()
-
-
-
-	if btn(0) then y=y-1 end
-	if btn(1) then y=y+1 end
-	if btn(2) then x=x-1 end
-	if btn(3) then x=x+1 end
-
-	cls(13)
-	spr(1+t%60//30*2,x,y,14,3,0,0,2,2)
-	print("HELLO WORLD!",84,84)
-	t=t+1
-end
 
 Engine = {_init = {}, _update = {}, _draw = {}, _uidraw = {}}
 
